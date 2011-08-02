6ec916ad8336f21e1fa27d7b5568298f210c8768
Mark Cerqueira <mark@smule.com>
2011-08-02 12:39:20 -0700

    Better separation of path to bank files and bank files.

--- bank_parser/bank_parser.rb
+++ bank_parser/bank_parser.rb
@@ -10,24 +10,23 @@ SERVER_PORT = 6449
 
 BANK_PATH = '/Users/Mark/Library/Application Support/Blizzard/StarCraft II/Accounts/65359401/1-S2-1-1685872/Banks'
 
+RESOURCES_BANK = 'tutorialBank1.SC2Bank'
+
 def sendEconomy
-  doc = Nokogiri::XML( open( BANK_PATH + '/tutorialBank1.SC2Bank' ) )
+  doc = Nokogiri::XML( open( BANK_PATH + '/' + RESOURCES_BANK ) )
 
   conn = UDPSocket.new
 
   for key in doc.css('Key')
-    puts key["name"].inspect
-    puts key.css('Value').first['int'].inspect
+    puts key["name"].inspect + ' : ' + key.css('Value').first['int'].inspect
 
     msg = Message.new( "/plorkCraft/test", "ss", key["name"], key.css('Value').first['int'] )
     conn.send msg.encode, 0, SERVER_HOST, SERVER_PORT  
   end  
 end
 
-FSSM.monitor( BANK_PATH, 'tutorialBank1.SC2Bank') do
-  update 
-  { 
-    |b, r| puts "Update in #{b} to #{r}"
+FSSM.monitor( BANK_PATH, RESOURCES_BANK ) do
+  update { |b, r| puts "Update in #{b} to #{r}"
     sendEconomy 
   }
   delete { |b, r| puts "Delete in #{b} to #{r}" }
