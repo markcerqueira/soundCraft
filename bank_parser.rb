8fbbbebdb01e30ff1869b899bfe73aa582c21599
Mark Cerqueira <mark.cerqueira@gmail.com>
2013-01-19 21:54:24 -0800

    Update XML bank parser to handle string data and update bank path

--- bank_parser.rb
+++ bank_parser.rb
@@ -10,12 +10,7 @@ SERVER_HOST = "localhost"
 SERVER_PORT = 6449
 
 # path to the folder containing all bank files created by Starcraft 2
-BANK_PATH = '/Users/Mark/Library/Application Support/Blizzard/StarCraft II/Accounts/65359401/1-S2-1-1685872/Banks'
-
-# filenames of banks that are used
-MINERAL_BANK = 'mineralBank.SC2Bank'
-VESPENE_BANK = 'vespeneBank.SC2Bank'
-SUPPLY_BANK = 'supplyBank.SC2Bank'
+BANK_PATH = '/Users/Mark/Library/Application Support/Blizzard/StarCraft II/Banks'
 
 # given filename identified by bankName, sends all key-value pairs in the bank file via OSC
 def sendBank( bankName )
@@ -23,10 +18,23 @@ def sendBank( bankName )
   conn = UDPSocket.new
 
   for key in doc.css('Key')
-    puts key["name"].inspect + ' : ' + key.css('Value').first['int'].inspect
-    msg = Message.new( "/lorkCraft/" + bankName, "ss", key["name"], key.css('Value').first['int'] )
-    conn.send msg.encode, 0, SERVER_HOST, SERVER_PORT  
+    keyName = key["name"]
+    
+    # parse as int first, then try parsing as string if that doesn't work
+    value = key.css('Value').first['int']
+    value = key.css('Value').first['string'] if value.nil?
+    
+    puts keyName + ' : ' + value
+    
+    if(value.nil?)
+      puts "Nil value for key named: " + keyName
+    else
+      msg = Message.new( "/lorkCraft/" + bankName, "ss", keyName, value )
+      conn.send msg.encode, 0, SERVER_HOST, SERVER_PORT
+    end
   end  
+  
+  puts ""
 end
 
 # File System State Monitor trigger
