8d23d7ed56b679aba58c1bf08da322a87e3e3e4d
Mark Cerqueira <mark.cerqueira@gmail.com>
2013-01-21 14:06:41 -0800

    Unit/building composition trigger updated with parser updates

--- bank_parser.rb
+++ bank_parser.rb
@@ -14,27 +14,63 @@ BANK_PATH = '/Users/Mark/Library/Application Support/Blizzard/StarCraft II/Banks
 POLLING_LATENCY_SECONDS = 0.05;
 
 # given filename identified by bankName, sends all key-value pairs in the bank file via OSC
-def sendBank( bankName )
+def sendBank(bankName)
+  # special bank handling functions
+  if(bankName == "unitsBuiltBank.SC2Bank")
+    processUnitsBuiltBank(bankName)
+    return
+  end
+
   doc = Nokogiri::XML( open( BANK_PATH + '/' + bankName ) )
   conn = UDPSocket.new
 
   for key in doc.css('Key')
     keyName = key["name"]
-    
+
     # parse as int first, then try parsing as string if that doesn't work
     value = key.css('Value').first['int']
     value = key.css('Value').first['string'] if value.nil?
-    
+
     puts keyName + ' : ' + value
-    
+
     if(value.nil?)
       puts "Nil value for key named: " + keyName
     else
       msg = Message.new( "/lorkCraft/" + bankName, "ss", keyName, value )
       conn.send msg.encode, 0, SERVER_HOST, SERVER_PORT
     end
-  end  
-  
+  end
+
+  puts ""
+end
+
+def processUnitsBuiltBank(bankName)
+  doc = Nokogiri::XML( open( BANK_PATH + '/' + bankName ) )
+  conn = UDPSocket.new
+
+  # hash to keep track of units, default to 0 so we don't need to check if the unit has already been added
+  unitHash = Hash.new
+  unitHash.default = 0;
+
+  # construct the hash
+  for key in doc.css('Key')
+    # note: we don't care about the "name" here as it's just an arbitrary unique integer
+
+    unitName = key.css('Value').first['string']
+
+    # remove the common prefix from the unit name passed
+    unitName.gsub!("Unit/Name/", "")
+
+    unitHash[unitName] = unitHash[unitName] + 1
+  end
+
+  unitHash.each_pair do |unitName, unitCount|
+    puts "#{unitName} : #{unitCount}"
+
+    msg = Message.new( "/lorkCraft/" + bankName, "si", unitName, unitCount )
+    conn.send msg.encode, 0, SERVER_HOST, SERVER_PORT
+  end
+
   puts ""
 end
 
@@ -42,6 +78,7 @@ Listen.to(BANK_PATH, :filter => /\.SC2Bank$/, :latency => POLLING_LATENCY_SECOND
   String absolutePath = modified.inspect
   absolutePath = added.inspect if absolutePath.nil?
 
+  # remove silly characters surrounding the path
   absolutePath.gsub!("[\"", "")
   absolutePath.gsub!("\"]", "")
 
