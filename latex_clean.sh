e4a5a3265a61953fcd0703fbba23dfe563875b14
Mark Cerqueira <mark.cerqueira@gmail.com>
2013-01-27 15:46:29 -0800

    Latex clean-up script

---
+++ latex_clean.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+rm -f *.aux
+rm -f *.glo
+rm -f *.idx
+rm -f *.log
+rm -f *.toc
+rm -f *.ist
+rm -f *.acn
+rm -f *.acr
+rm -f *.alg
+rm -f *.bbl
+rm -f *.blg
+rm -f *.dvi
+rm -f *.glg
+rm -f *.gls
+rm -f *.ilg
+rm -f *.ind
+rm -f *.lof
+rm -f *.lot
+rm -f *.maf
+rm -f *.mtc
+rm -f *.mtc1
+rm -f *.out
+rm -f *.synctex.gz
+rm -f *.bak
